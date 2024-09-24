430cd05fc98bb984d5915a14dc5b71f9b896f43b
Sean Pedersen <sean.pedersen(AT)gmail.com>
2024-09-24 14:19:18 +0200

    Add posthog

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '../styles/global.css'
 
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined') {
+  posthog.init('phc_9XPlyPALuefIMAMSfsvBk4jVuSelJyjl7xwhXigkHAP', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only',
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    }
+  })
+}
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  )
 }
