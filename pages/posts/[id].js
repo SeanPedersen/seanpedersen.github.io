913e5543faee1b8fa8e2b8d450c3dbd57fe9b7a2
Sean Pedersen <sean.pedersen(AT)gmail.com>
2024-12-09 21:10:14 +0100

    Add basic code block support

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,9 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { Prism } from 'prism-react-renderer'
+import codeStyles from '../../styles/code.module.css'
+
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -22,19 +25,44 @@ export async function getStaticPaths() {
 }
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-      </Layout>
-    )
+  const escapeHtml = (unsafe) => {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
   }
-  
+
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div
+          className="markdown-content"
+          dangerouslySetInnerHTML={{
+            __html: postData.contentHtml
+              .replace(
+                /<pre><code>(.*?)<\/code><\/pre>/gs,
+                (_, code) => `
+                  <div class="${codeStyles.codeBlock}">
+                    <pre>${escapeHtml(code)}</pre>
+                  </div>
+                `
+              )
+              .replace(
+                /<code>(.*?)<\/code>/g,
+                (_, code) => `<code class="${codeStyles.inlineCode}">${escapeHtml(code)}</code>`
+              )
+          }}
+        />
+      </article>
+    </Layout>
+  )
+}
