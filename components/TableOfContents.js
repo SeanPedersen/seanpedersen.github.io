09763555921f802c53aef2ad3e0a2e036a76f005
Sean Pedersen <sean.pedersen(AT)gmail.com>
2024-12-09 22:01:48 +0100

    Add table of content

---
+++ components/TableOfContents.js
@@ -0,0 +1,41 @@
+import styles from './TableOfContents.module.css'
+
+export default function TableOfContents({ content }) {
+    const getHeadings = (content) => {
+        const regex = /<h([1-6]).*?>(.*?)<\/h[1-6]>/g;
+        const headings = [];
+        let match;
+
+        while ((match = regex.exec(content)) !== null) {
+            headings.push({
+                level: Number(match[1]),
+                text: match[2].replace(/<[^>]*>/g, ''),
+                id: match[2].toLowerCase().replace(/[^a-z0-9]+/g, '-')
+            });
+        }
+
+        return headings;
+    };
+
+    const headings = getHeadings(content);
+
+    if (headings.length === 0) {
+        return null;
+    }
+
+    return (
+        <nav className={styles.toc}>
+            <h2>Table of Contents</h2>
+            <ul>
+                {headings.map((heading, index) => (
+                    <li
+                        key={index}
+                        style={{ paddingLeft: `${(heading.level - 1) * 16}px` }}
+                    >
+                        <a href={`#${heading.id}`}>{heading.text}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,12 @@
 import Layout from '../../components/layout'
+import styles from '../../components/layout.module.css'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 import { Prism } from 'prism-react-renderer'
 import codeStyles from '../../styles/code.module.css'
+import TableOfContents from '../../components/TableOfContents'
 
 
 export async function getStaticProps({ params }) {
@@ -34,35 +36,50 @@ export default function Post({ postData }) {
       .replace(/'/g, "&#039;");
   }
 
+  const addHeadingIds = (content) => {
+    return content.replace(
+      /<h([1-6])>(.*?)<\/h[1-6]>/g,
+      (match, level, text) => {
+        const id = text.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+        return `<h${level} id="${id}">${text}</h${level}>`;
+      }
+    );
+  };
+
+  const hasTableOfContents = postData.contentHtml.includes('<h2') || postData.contentHtml.includes('<h3');
+
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div
-          className="markdown-content"
-          dangerouslySetInnerHTML={{
-            __html: postData.contentHtml
-              .replace(
-                /<pre><code>(.*?)<\/code><\/pre>/gs,
-                (_, code) => `
-                  <div class="${codeStyles.codeBlock}">
-                    <pre>${escapeHtml(code)}</pre>
-                  </div>
-                `
-              )
-              .replace(
-                /<code>(.*?)<\/code>/g,
-                (_, code) => `<code class="${codeStyles.inlineCode}">${escapeHtml(code)}</code>`
-              )
-          }}
-        />
-      </article>
+      <div className={`${styles.main} ${styles.postPage} ${!hasTableOfContents ? styles.noToc : ''}`}>
+        {hasTableOfContents && <TableOfContents content={postData.contentHtml} />}
+        <article className={utilStyles.postContainer}>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div
+            className="markdown-content"
+            dangerouslySetInnerHTML={{
+              __html: addHeadingIds(postData.contentHtml)
+                .replace(
+                  /<pre><code>(.*?)<\/code><\/pre>/gs,
+                  (_, code) => `
+                    <div class="${codeStyles.codeBlock}">
+                      <pre>${escapeHtml(code)}</pre>
+                    </div>
+                  `
+                )
+                .replace(
+                  /<code>(.*?)<\/code>/g,
+                  (_, code) => `<code class="${codeStyles.inlineCode}">${escapeHtml(code)}</code>`
+                )
+            }}
+          />
+        </article>
+      </div>
     </Layout>
   )
 }
