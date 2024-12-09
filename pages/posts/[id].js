fccae9240babe5bff82ea042e567ce8cc7040eb4
Sean Pedersen <sean.pedersen(AT)gmail.com>
2024-12-09 22:17:34 +0100

    Remove prism

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,7 +4,6 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
-import { Prism } from 'prism-react-renderer'
 import codeStyles from '../../styles/code.module.css'
 import TableOfContents from '../../components/TableOfContents'
 
