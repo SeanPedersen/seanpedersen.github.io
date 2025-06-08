6532a444bafb7d3d4a2daaddc4ae95c31df792e0
Sean Pedersen <seanpedersen96@protonmail.com>
2025-06-09 00:41:11 +0200

    Fix theme loading (remove flickering for light mode on page load)

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,26 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  try {
+                    var storedTheme = document.cookie
+                      .split('; ')
+                      .find(function(row) { return row.startsWith('theme='); });
+                    
+                    var theme = storedTheme ? storedTheme.split('=')[1] : 
+                      (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark');
+                    
+                    if (theme === 'light') {
+                      document.body.classList.add('light-theme');
+                    }
+                  } catch (e) {}
+                })();
+              `
+            }}
+          />
           <Main />
           <NextScript />
         </body>
